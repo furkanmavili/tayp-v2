11172902ac43c0c9931a712dfc342459defecf39
furkanmavili <furkanmavilix@gmail.com>
2022-03-07 22:51:41 +0300

    chore: add padding to container

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import Footer from "./components/layout/Footer.vue";
 
 <template>
   <div class="h-full bg-slate-100 text-gray-950 dark:text-slate-100 dark:bg-gray-950">
-    <div class="container mx-auto max-w-4xl py-6 flex flex-col h-full">
+    <div class="container mx-auto max-w-4xl py-6 px-5 flex flex-col h-full">
       <Header />
       <div class="py-6 flex-1">
         <RouterView />
