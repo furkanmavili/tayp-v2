81068104db9417c6bc88ed7485e98c3437123737
furkanmavili <furkanmavilix@gmail.com>
2022-03-07 22:50:49 +0300

    chore: update score store

--- src/stores/score.js
+++ src/stores/score.js
@@ -1,17 +1,23 @@
 import { defineStore } from "pinia";
 
+const initialState = {
+  correct: 0,
+  wrong: 0,
+  wpm: null,
+  raw_wpm: null,
+  show_results: false,
+};
 export const useScoreStore = defineStore({
   id: "score",
-  state: () => ({
-    correct: 0,
-    wrong: 0,
-    wpm: null,
-    raw_wpm: null,
-  }),
-  // getters: {
-  //   doubleCount: (state) => state.counter * 2,
-  // },
+  state: () => initialState,
   actions: {
+    reset() {
+      this.correct = 0;
+      this.wrong = 0;
+      // this.wpm = null;
+      // this.raw_wpm = null;
+      this.show_results = false;
+    },
     addCorrect() {
       this.correct++;
     },
@@ -23,6 +29,9 @@ export const useScoreStore = defineStore({
     },
     setRawWpm(result) {
       this.raw_wpm = result;
+    },
+    setShowResults(result) {
+      this.show_results = result;
     },
   },
 });
